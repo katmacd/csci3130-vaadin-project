96be27f560a2cd2cdc0ffc335b04e5f51140cb5d
Jonathan <jon_mcdevitt@hotmail.com>
2017-01-27 08:42:24 -0400

    # Author: Primary programmer of this commit Author: Jonathan
    # Partner: Author's pair programming partner
    Partner: Taylor
    
    # Summary: One-line high-level description of problem being solved
    Summary: Adding a test class as a proof of concept
    
    # Description: Description of problem solution
    Description: Summary sufficient

---
+++ test/com/project/MyUITest.java
@@ -0,0 +1,24 @@
+package com.project;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Owner on 2017-01-27.
+ */
+public class MyUITest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void init() throws Exception {
+
+    }
+
+}
